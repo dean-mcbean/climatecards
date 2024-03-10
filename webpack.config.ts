c34c2f8287da2ff4f329c5072917a110bf29447c
Dean Walker <dean.w.mcbean@gmail.com>
2024-03-11 00:15:40 +1300

    Fixed file names, set build command

--- webpack.config.ts
+++ webpack.config.ts
@@ -37,7 +37,7 @@ const config: Configuration = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../../www/resilient-cities"),
   },
   plugins: [
     new CopyWebpackPlugin({
